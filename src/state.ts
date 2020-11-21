1bff8449596e3f996993fa27dc8f671b51d5cad4
Steve Ruiz <steveruizok@gmail.com>
2020-11-21 12:28:28 +0000

    tweaks

--- src/state.ts
+++ src/state.ts
@@ -1,7 +1,7 @@
-import chalk from "chalk";
-import { createState, S } from "@state-designer/react";
-import * as Static from "./static";
-import * as Types from "./types";
+import chalk from "chalk"
+import { createState, S } from "@state-designer/react"
+import * as Static from "./static"
+import * as Types from "./types"
 
 const state = createState({
   id: "global",
@@ -232,33 +232,33 @@ const state = createState({
   },
   results: {
     selectedTest(data) {
-      return data.tests.find((t) => t.id === data.selectedTestId);
+      return data.tests.find((t) => t.id === data.selectedTestId)
     },
     selectedUser(data) {
-      return data.users.find((t) => t.id === data.selectedUserId);
+      return data.users.find((t) => t.id === data.selectedUserId)
     },
   },
   conditions: {
     selectedTestIsDraft(data, _, selectedTest) {
-      if (selectedTest === undefined) return false;
-      return selectedTest.status === "draft";
+      if (selectedTest === undefined) return false
+      return selectedTest.status === "draft"
     },
     selectedTestIsComplete(data, _, selectedTest) {
-      if (selectedTest === undefined) return false;
-      return selectedTest.status === "complete";
+      if (selectedTest === undefined) return false
+      return selectedTest.status === "complete"
     },
     selectedTestIsInProgress(data, _, selectedTest) {
-      if (selectedTest === undefined) return false;
-      return selectedTest.status === "inProgress";
+      if (selectedTest === undefined) return false
+      return selectedTest.status === "inProgress"
     },
   },
   actions: {
     setSelectedTest(data, payload = {}) {
-      const { test } = payload;
-      data.selectedTestId = test.id;
+      const { test } = payload
+      data.selectedTestId = test.id
     },
     clearSelectedTest(data) {
-      data.selectedTestId = undefined;
+      data.selectedTestId = undefined
     },
     createDraft(data, { name }: { name: string }) {
       const newTest: Types.UserTest = {
@@ -279,85 +279,83 @@ const state = createState({
         },
         survey: [],
         goals: [],
-      };
+      }
 
-      data.tests.push(newTest);
-      data.selectedTestId = newTest.id;
+      data.tests.push(newTest)
+      data.selectedTestId = newTest.id
     },
     addUserToTest(
       data,
       { user }: { user: Types.User },
       selectedTest: Types.UserTest
     ) {
-      if (!selectedTest) return;
-      selectedTest.invitedUsers.push(user);
+      if (!selectedTest) return
+      selectedTest.invitedUsers.push(user)
     },
     duplicateDraft(data, _, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
+      if (!selectedTest) return
       const newTest: Types.UserTest = {
         ...selectedTest,
         id: "test" + Date.now(),
         name: selectedTest.name + " Copy",
-      };
+      }
 
-      data.tests.push(newTest);
-      data.selectedTestId = newTest.id;
+      data.tests.push(newTest)
+      data.selectedTestId = newTest.id
     },
     deleteDraft(data, _, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
-      const index = data.tests.findIndex((t) => t.id === selectedTest.id);
-      data.selectedTestId = undefined;
-      data.tests.splice(index, 1);
+      if (!selectedTest) return
+      const index = data.tests.findIndex((t) => t.id === selectedTest.id)
+      data.selectedTestId = undefined
+      data.tests.splice(index, 1)
     },
     completeDraft(data, _, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
-      selectedTest.status = "inProgress";
+      if (!selectedTest) return
+      selectedTest.status = "inProgress"
     },
     toggleMultipleTries(data, _, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
+      if (!selectedTest) return
       selectedTest.options.allowMultipleTries = !selectedTest.options
-        .allowMultipleTries;
+        .allowMultipleTries
     },
     toggleTestDataProperty(data, { property }, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
-      selectedTest.options.data[property] = !selectedTest.options.data[
-        property
-      ];
+      if (!selectedTest) return
+      selectedTest.options.data[property] = !selectedTest.options.data[property]
     },
     setDraftName(data, payload, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
-      const { value = "" } = payload;
-      selectedTest.name = value;
+      if (!selectedTest) return
+      const { value = "" } = payload
+      selectedTest.name = value
     },
     setTestDuration(data, payload = {}, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
-      const { value } = payload;
-      selectedTest.options.testDuration = value * (1000 * 60 * 24);
+      if (!selectedTest) return
+      const { value } = payload
+      selectedTest.options.testDuration = value * (1000 * 60 * 24)
     },
     removeUserFromDraft(data, _, selectedTest: Types.UserTest) {
-      if (!selectedTest) return;
-      console.log("removing", data.selectedUserId);
+      if (!selectedTest) return
+      console.log("removing", data.selectedUserId)
       const index = selectedTest.invitedUsers.findIndex(
         (t) => t.id === data.selectedUserId
-      );
-      selectedTest.invitedUsers.splice(index, 1);
+      )
+      selectedTest.invitedUsers.splice(index, 1)
     },
     // Selected User
     setSelectedUserId(data, { user }: { user: Types.User }) {
-      data.selectedUserId = user.id;
+      data.selectedUserId = user.id
     },
     clearSelectedUserId(data) {
-      data.selectedUserId = "";
+      data.selectedUserId = ""
     },
   },
   values: {
     selectedTest(data) {
-      return data.tests.find((t) => t.id === data.selectedTestId);
+      return data.tests.find((t) => t.id === data.selectedTestId)
     },
   },
-});
+})
 
-export default state;
+export default state
 
 // state.onUpdate((update) => {
 // let colWidths = [0]
@@ -423,28 +421,28 @@ export default state;
 // )
 // })
 
-type NodeType = "leaf" | "branch" | "root";
+type NodeType = "leaf" | "branch" | "root"
 
 class Grid {
-  rows = [] as { char: string; node?: TNode }[][];
-  width = 0;
-  height = 0;
+  rows = [] as { char: string; node?: TNode }[][]
+  width = 0
+  height = 0
 
   chars = {
     active: ["┌", "─", "┒", "┃", "┛", "━", "┕", "│"],
     inactive: ["┌", "─", "┐", "│", "┘", "─", "└", "│"],
     root: ["┌", "╌", "┐", "╎", "┘", "╌", "└", "╎"],
-  };
+  }
 
   setSize(width: number, height: number) {
     this.rows = Array.from(Array(height)).map(() =>
       Array.from(Array(width)).map(() => ({ char: " ", node: undefined }))
-    );
+    )
   }
 
   insert(char: string, col: number, row: number, node: TNode) {
-    if (this.rows[row] === undefined) this.rows[row] = [];
-    this.rows[row][col] = { char, node };
+    if (this.rows[row] === undefined) this.rows[row] = []
+    this.rows[row][col] = { char, node }
   }
 
   drawRect(
@@ -455,43 +453,43 @@ class Grid {
     style: "active" | "inactive" | "root",
     node: TNode
   ) {
-    let i: number;
-    const chars = this.chars[style];
-    this.insert(chars[0], x, y, node);
-    this.insert(chars[2], x + width, y, node);
-    this.insert(chars[4], x + width, y + height, node);
-    this.insert(chars[6], x, y + height, node);
+    let i: number
+    const chars = this.chars[style]
+    this.insert(chars[0], x, y, node)
+    this.insert(chars[2], x + width, y, node)
+    this.insert(chars[4], x + width, y + height, node)
+    this.insert(chars[6], x, y + height, node)
     for (i = 1; i < width; i++) {
-      this.insert(chars[1], x + i, y, node);
-      this.insert(chars[5], x + i, y + height, node);
+      this.insert(chars[1], x + i, y, node)
+      this.insert(chars[5], x + i, y + height, node)
     }
     for (i = 1; i < height; i++) {
-      this.insert(chars[7], x, y + i, node);
-      this.insert(chars[3], x + width, y + i, node);
+      this.insert(chars[7], x, y + i, node)
+      this.insert(chars[3], x + width, y + i, node)
     }
   }
 
   drawText(text: string, x: number, y: number, node: TNode) {
     for (let i = 0; i < text.length; i++) {
-      this.insert(text[i], x + i, y, node);
+      this.insert(text[i], x + i, y, node)
     }
   }
 
   drawNode(node: TNode) {
-    const { x, y, width, height, type, state, name } = node;
+    const { x, y, width, height, type, state, name } = node
     const style =
-      type === "root" ? "root" : state.active ? "active" : "inactive";
+      type === "root" ? "root" : state.active ? "active" : "inactive"
     if (node.hasChildren) {
-      this.drawRect(x, y, width, height, style, node);
-      this.insert(" ", x + 1, y, node);
-      this.insert(" ", x + name.length + 2, y, node);
-      this.drawText(name, x + 2, y, node);
+      this.drawRect(x, y, width, height, style, node)
+      this.insert(" ", x + 1, y, node)
+      this.insert(" ", x + name.length + 2, y, node)
+      this.drawText(name, x + 2, y, node)
     } else {
-      this.drawText(name, x, y, node);
+      this.drawText(name, x, y, node)
     }
 
     for (let child of node.children) {
-      this.drawNode(child);
+      this.drawNode(child)
     }
   }
 
@@ -510,110 +508,110 @@ class Grid {
             .join("")
         )
         .join("\n")
-    );
+    )
   }
 
   init(node: TNode) {
-    node.moveTo(0, 0);
-    this.setSize(node.width, node.height);
-    this.drawNode(node);
-    this.render();
+    node.moveTo(0, 0)
+    this.setSize(node.width, node.height)
+    this.drawNode(node)
+    this.render()
   }
 }
 
-const grid = new Grid();
+const grid = new Grid()
 
 class TNode {
-  name: string;
-  parent: TNode | undefined;
-  state: S.State<any, any>;
-  children: TNode[];
-  x = 0;
-  y = 0;
+  name: string
+  parent: TNode | undefined
+  state: S.State<any, any>
+  children: TNode[]
+  x = 0
+  y = 0
 
   constructor(state: S.State<any, any>, parent?: TNode) {
-    this.state = state;
-    this.name = state.name;
-    this.parent = parent;
-    this.children = Object.values(state.states).map((s) => new TNode(s, this));
+    this.state = state
+    this.name = state.name
+    this.parent = parent
+    this.children = Object.values(state.states).map((s) => new TNode(s, this))
   }
 
   get maxX() {
-    return this.x + this.width;
+    return this.x + this.width
   }
 
   get maxY() {
-    return this.y + this.height;
+    return this.y + this.height
   }
 
   get width() {
     if (!this.hasChildren) {
-      return this.name.length;
+      return this.name.length
     }
 
     let cx = Math.max(
       this.x + this.name.length + 5,
       ...this.children.map((c) => c.maxX)
-    );
+    )
 
-    if (this.children.find((c) => c.type === "branch")) cx++;
+    if (this.children.find((c) => c.type === "branch")) cx++
 
-    cx++;
+    cx++
 
-    return cx - this.x;
+    return cx - this.x
   }
 
   get height() {
     if (!this.hasChildren) {
-      return 1;
+      return 1
     }
 
-    let cy = Math.max(...this.children.map((c) => c.maxY));
-    if (cy > this.y + 3) cy++;
+    let cy = Math.max(...this.children.map((c) => c.maxY))
+    if (cy > this.y + 3) cy++
 
-    return cy - this.y;
+    return cy - this.y
   }
 
   get hasChildren() {
-    return this.children.length > 0;
+    return this.children.length > 0
   }
 
   get type(): NodeType {
-    if (!this.parent) return "root";
-    if (this.children.length === 0) return "leaf";
-    return "branch";
+    if (!this.parent) return "root"
+    if (this.children.length === 0) return "leaf"
+    return "branch"
   }
 
   moveTo(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+    this.x = x
+    this.y = y
 
-    let sx = x + 2;
-    let sy = y + 1;
+    let sx = x + 2
+    let sy = y + 1
 
     for (let i = 0; i < this.children.length; i++) {
-      const child = this.children[i];
+      const child = this.children[i]
 
-      child.moveTo(sx, sy);
+      child.moveTo(sx, sy)
 
-      let mx = child.width + (child.type === "leaf" ? 1 : 2);
-      let my = child.height + (child.type === "leaf" ? 0 : 1);
+      let mx = child.width + (child.type === "leaf" ? 1 : 2)
+      let my = child.height + (child.type === "leaf" ? 0 : 1)
 
       if (i % 2 === 0) {
-        sx += mx;
+        sx += mx
       } else {
-        sx = x + 2;
-        sy += my;
+        sx = x + 2
+        sy += my
       }
     }
   }
 }
 
-const tTree = new TNode(state.stateTree);
-grid.init(tTree);
-grid.render();
+// const tTree = new TNode(state.stateTree)
+// grid.init(tTree)
+// grid.render()
 
-state.onUpdate((update) => {
-  grid.render();
-  console.log("Last Event:", update.log[0]);
-});
+// state.onUpdate((update) => {
+//   grid.render();
+//   console.log("Last Event:", update.log[0]);
+// });
